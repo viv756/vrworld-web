904061039821b8f0d6ac7d92871323c7ab38583b
*vivek <vrvivekofficial@gmail.com>
2024-05-22 12:08:52 +0530

    quotes updated

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,14 +1,12 @@
-
 const Quotes = () => {
   return (
-    <>
       <div className="container py-14 px-4 ">
         <h1 className="text-2xl sm:text-4xl font-bold max-w-[650px] mx-auto text-center font-parisienne duration-300">
-        "Virtual reality is a medium, a means by which humans can share ideas and experiences."
-    </h1>
+          "Virtual reality is a medium, a means by which humans can share ideas
+          and experiences."
+        </h1>
       </div>
-    </>
-  )
-}
+  );
+};
 
-export default Quotes
+export default Quotes;
